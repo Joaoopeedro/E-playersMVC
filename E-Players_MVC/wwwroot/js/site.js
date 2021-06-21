cbd1fbc3d326f16464c3a896828d2ac763dcd01f
joao <joaopedro37@gmail.com>
2021-06-21 19:30:28 -0300

    semi-finalizado

--- E-Players_MVC/wwwroot/js/site.js
+++ E-Players_MVC/wwwroot/js/site.js
@@ -2,3 +2,32 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+var menu = document.getElementById("Link");
+        function botaodoMenu() {
+            if (screen.width <= 768) {
+                if (menu.style.display === "flex") {
+                    // esconder o menu
+                    menu.style.display = "none";
+                } else {
+                    // Mostrar o menu 
+                    menu.style.display = "flex";
+                }
+            }
+        }
+        function fecharMenu() {
+            var fechar_menu =
+                document.getElementById("Link");
+            if (fechar_menu.style.display === "flex") {
+                // esconder menu
+                fechar_menu.style.display = "none";
+            }
+        }
+        window.onresize = () => {
+            if (screen.width > 768) {
+                menu.style.display = "flex";
+            } else {
+                menu.style.display = "none";
+            }
+        }
